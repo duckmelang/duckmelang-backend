5803cc2c55c6d5622013cd73ba53bd985ceda726
YangYoonseo <yys0847@naver.com>
2025-01-09 12:44:49 +0900

    Feat: 프로젝트 초기 설정
    Spring Boot 프로젝트 초기 설정
    -Swagger 설정 추가
    -JPA, MySql 의존성 추가
    -로컬 DB(application.yml, application-local.yml) 설정

---
+++ src/main/java/umc/duckmelang/DuckmelangApplication.java
@@ -0,0 +1,13 @@
+package umc.duckmelang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuckmelangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuckmelangApplication.class, args);
+	}
+
+}
