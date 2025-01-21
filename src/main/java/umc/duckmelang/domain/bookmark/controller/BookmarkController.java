5587b9fe38a886753dac016c2c9c51de790b41aa
YangYoonseo <yys0847@naver.com>
2025-01-21 14:12:11 +0900

    Feat: @CommonApiResponses 구현 기존 @ApiResponses 대신 Custom Annotation 생성 후 적용 Fixes: #18

--- src/main/java/umc/duckmelang/domain/bookmark/controller/BookmarkController.java
+++ src/main/java/umc/duckmelang/domain/bookmark/controller/BookmarkController.java
@@ -11,6 +11,7 @@ import umc.duckmelang.domain.idolcategory.validation.annotation.ExistIdol;
 import umc.duckmelang.domain.post.converter.PostConverter;
 import umc.duckmelang.domain.post.domain.Post;
 import umc.duckmelang.domain.post.dto.PostResponseDto;
+import umc.duckmelang.global.annotations.CommonApiResponses;
 import umc.duckmelang.global.apipayload.ApiResponse;
 
 @RestController
@@ -22,6 +23,7 @@ public class BookmarkController {
     private final BookmarkQueryService bookmarkQueryService;
 
     @GetMapping("/{memberId}")
+    @CommonApiResponses
     @Operation(summary = "스크랩 내역 조회 API", description = "개인 스크랩 내역 조회 API입니다. 페이징을 포함하며 한 페이지 당 10개 게시글을 보여줍니다. query String으로 page 번호를 주세요. page 번호는 0부터 시작합니다")
     public ApiResponse<PostResponseDto.PostPreviewListDto> getBookmarks(@PathVariable(name="memberId") Long memberId, @RequestParam(name = "page") Integer page) {
         Page<Post> postList = bookmarkQueryService.getBookmarks(memberId, page);
--- src/main/java/umc/duckmelang/domain/post/controller/PostController.java
+++ src/main/java/umc/duckmelang/domain/post/controller/PostController.java
@@ -16,6 +16,7 @@ import umc.duckmelang.domain.post.dto.PostResponseDto;
 import umc.duckmelang.domain.post.service.PostCommandService;
 import umc.duckmelang.domain.post.service.PostCommandServiceImpl;
 import umc.duckmelang.domain.post.service.PostQueryService;
+import umc.duckmelang.global.annotations.CommonApiResponses;
 import umc.duckmelang.global.apipayload.ApiResponse;
 
 @RestController
@@ -28,6 +29,7 @@ public class PostController {
     private final PostCommandService postCommandService;
 
     @GetMapping("/")
+    @CommonApiResponses
     @Operation(summary = "홈화면 게시글 전체 조회 API", description = "조건 없이 모든 게시글을 조회하는 API 입니다. 페이징을 포함하며 한 페이지 당 10개 게시글을 보여줍니다. query String으로 page 번호를 주세요. page 번호는 0부터 시작합니다")
     public ApiResponse<PostResponseDto.PostPreviewListDto> getPostList (@RequestParam(name = "page") Integer page){
         if(page<0){
@@ -38,6 +40,7 @@ public class PostController {
     }
 
     @GetMapping("/idol/{idolId}")
+    @CommonApiResponses
     @Operation(summary = "홈화면 게시글 아이돌 기반 조회 API", description = "해당하는 아이돌의 글만 조회하는 API 입니다. 페이징을 포함하며 한 페이지 당 10개 게시글을 보여줍니다. query String으로 page 번호를 주세요. page 번호는 0부터 시작합니다")
     @Parameters({
             @Parameter(name = "idolId", description = "아이돌 Id, path variable 입니다!")
@@ -51,6 +54,7 @@ public class PostController {
     }
 
     @GetMapping("/{postId}")
+    @CommonApiResponses
     @Operation(summary = "게시글 상세 조회 API", description = "홈화면에서 게시글 1개 클릭시 자세히 보여주는 API입니다. 성별은 true일때 남자, false일때 여자입니다. 스크랩, 채팅, 조회수, 후기평 아직 만들지 않음")
     @Parameters({@Parameter(name = "postId", description = "게시글 Id, path variable 입니다")})
     public ApiResponse<PostResponseDto.PostDetailDto> getPostDetail (@PathVariable(name="postId") Long postId){
@@ -60,6 +64,7 @@ public class PostController {
     }
 
     @PostMapping("/{memberId}")
+    @CommonApiResponses
     @Operation(summary = "게시글 작성 API", description = "게시글 쓰기 API입니다. ")
     public ApiResponse<PostResponseDto.PostJoinResultDto> joinPost (@PathVariable(name="memberId") Long memberId, @RequestBody @Valid PostRequestDto.PostJoinDto request){
         Post post = postCommandService.joinPost(request, memberId);
@@ -67,6 +72,7 @@ public class PostController {
     }
 
     @GetMapping("/search")
+    @CommonApiResponses
     @Operation(summary = "게시글 검색 API", description = "게시글 검색 API입니다. title 기준으로 검색합니다. 페이징을 포함하며 한 페이지 당 10개 게시글을 보여줍니다. query String으로 page 번호를 주세요. page 번호는 0부터 시작합니다")
     public ApiResponse<PostResponseDto.PostPreviewListDto> getPostListByTitle (@RequestParam(name = "page") Integer page, @RequestParam(name="searchKeyword") String searchKeyword){
         Page<Post> postList = postQueryService.getPostListByTitle(searchKeyword, page);
