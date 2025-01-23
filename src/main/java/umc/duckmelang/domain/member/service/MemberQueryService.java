b09ff9418ca92c3acb83b26b14ae111568baccb2
Sieun Park <park12010@naver.com>
2025-01-23 19:57:32 +0900

    Feat: Member 나이 계산 서비스 메서드 구현
     회원 나이 계산 로직도 서비스 곳곳에 사용되니 하나의 서비스로 분리
    일단 만 나이를 바탕으로 계산하도록 구현
    
    Fixes: #20

--- src/main/java/umc/duckmelang/domain/member/service/MemberQueryService.java
+++ src/main/java/umc/duckmelang/domain/member/service/MemberQueryService.java
@@ -1,4 +1,5 @@
 package umc.duckmelang.domain.member.service;
 
 public interface MemberQueryService {
+    public int calculateMemberAge(Long userId);
 }
--- src/main/java/umc/duckmelang/domain/member/service/MemberQueryServiceImpl.java
+++ src/main/java/umc/duckmelang/domain/member/service/MemberQueryServiceImpl.java
@@ -1,4 +1,47 @@
 package umc.duckmelang.domain.member.service;
 
-public class MemberQueryServiceImpl {
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import umc.duckmelang.domain.member.domain.Member;
+import umc.duckmelang.domain.member.repository.MemberRepository;
+import umc.duckmelang.global.apipayload.code.status.ErrorStatus;
+import umc.duckmelang.global.apipayload.exception.handler.MemberHandler;
+
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+public class MemberQueryServiceImpl implements MemberQueryService{
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원 ID로 회원의 만 나이를 계산하는 서비스 메서드
+     * @param memberId 회원 ID(Long)
+     * @return 현재 나이(int)
+     */
+    @Override
+    @Transactional
+    public int calculateMemberAge(Long memberId) {
+
+        // 회원 정보 조회 및 유효성 검증
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberHandler(ErrorStatus.MEMBER_NOT_FOUND));
+
+        // 생년월일 가져오기
+        LocalDate birth = member.getBirth();
+
+        // 현재 날짜 가져오기
+        LocalDate today = LocalDate.now();
+
+        // 현재 연도와 태어난 연도의 차이를 계산
+        int age = today.getYear() - birth.getYear();
+
+        // 생일이 올해 아직 지나지 않았다면 나이를 1 줄임
+        if (today.isBefore(birth.withYear(today.getYear()))) {
+            age--;
+        }
+        return age;
+    }
 }
