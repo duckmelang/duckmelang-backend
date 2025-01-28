aec34412411ea6b2b15f7616602272f4453f482a
erika0915 <soohee2001@gmail.com>
2025-01-28 16:45:19 +0900

    Chore: controller에 설명 추가

--- src/main/java/umc/duckmelang/domain/auth/sms/SmsController.java
+++ src/main/java/umc/duckmelang/domain/auth/sms/SmsController.java
@@ -1,5 +1,6 @@
 package umc.duckmelang.domain.auth.sms;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,12 +14,14 @@ import umc.duckmelang.global.apipayload.ApiResponse;
 public class SmsController {
     private final SmsService smsService;
 
+    @Operation(summary = "문자 전송 API", description = "사용자의 휴대폰 번호로 인증번호를 전송하는 API입니다.")
     @PostMapping("/sms/send")
     public ApiResponse<String> sendSms(@RequestBody SmsRequestDto smsRequestDto){
         smsService.sendSms(smsRequestDto);
         return ApiResponse.onSuccess("문자 전송이 완료되었습니다.");
     }
 
+    @Operation(summary = "문자 인증 API", description = "사용자가 입력한 인증번호를 검증하는 API입니다.")
     @PostMapping("/sms/verify")
     public ApiResponse<String> verifyCode(@RequestBody SmsVerifyDto smsVerifyDto){
         boolean verify = smsService.verifyCode(smsVerifyDto);
