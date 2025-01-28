30b0a143563c12e262fe369497f8df89cffa50ad
sjw <shinjw0131@gmail.com>
2025-01-29 01:31:52 +0900

    chore : change for CI/CD

--- src/test/java/umc/duckmelang/DuckmelangApplicationTests.java
+++ src/test/java/umc/duckmelang/DuckmelangApplicationTests.java
@@ -3,7 +3,7 @@ package umc.duckmelang;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 class DuckmelangApplicationTests {
 
 	@Test
