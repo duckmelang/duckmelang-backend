be4987f07b69cc6fb0431a7153d8eb941d603d7d
YangYoonseo <yys0847@naver.com>
2025-01-29 13:38:31 +0900

    Refacotr: 후기글 작성 페이지 내 반환값 변경 가장 최신의 applicationId만 반환하는 것이 아닌, 이전의 것들도 list로 반환한다. Resolves: #32

--- src/main/java/umc/duckmelang/domain/review/controller/ReviewController.java
+++ src/main/java/umc/duckmelang/domain/review/controller/ReviewController.java
@@ -26,6 +26,7 @@ import umc.duckmelang.global.apipayload.exception.ApplicationException;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -46,17 +47,19 @@ public class ReviewController {
 
     @GetMapping("/information")
     @CommonApiResponses
-    @Operation(summary = "후기글 작성 페이지 내 관련 정보 조회 API", description = "후기글 작성 페이지에서 applicationId 외에 유저네임, 게시글 제목, 행사 날짜 등 정보를 보여주는 API 입니다. memberId를 requestParam으로 넣어주세요. myId는 추후 JWT 추출 예정")
-    public ApiResponse<ReviewResponseDto.ReviewInformationDto> getReviewInformation(@ExistMember @RequestParam(name="memberId") Long memberId, @RequestParam(name="myId") Long myId){
+    @Operation(summary = "후기글 작성 페이지 내 관련 정보 조회 API", description = "후기글 작성 페이지에서 applicationId 외에 유저네임, 게시글 제목, 행사 날짜 등 정보를 보여주는 API 입니다. 최신 순으로 정렬되어 list로 내보냅니다. memberId를 requestParam으로 넣어주세요. myId는 추후 JWT 추출 예정")
+    public ApiResponse<List<ReviewResponseDto.ReviewInformationDto>> getReviewInformation(@ExistMember @RequestParam(name="memberId") Long memberId, @RequestParam(name="myId") Long myId){
         List<Application> applications = reviewQueryService.getReviewInformation(myId, memberId);
 
-        // 첫 번째 Application만 사용
         if (applications.isEmpty()) {
             throw new ApplicationException(ErrorStatus.APPLICATION_NOT_FOUND);
         }
-        Application application = applications.get(0);
 
-        return ApiResponse.onSuccess(ReviewConverter.reviewInformationDto(application));
+        List<ReviewResponseDto.ReviewInformationDto> reviewInformationDtos = applications.stream()
+                .map(ReviewConverter::reviewInformationDto)
+                .collect(Collectors.toList());
+
+        return ApiResponse.onSuccess(reviewInformationDtos);
     }
 
 }
