692c53942103990c6d04f404c7e2a8e1eb676208
YangYoonseo <yys0847@naver.com>
2025-01-31 21:26:28 +0900

    Feat: Notification, NotificationSetting Entity 구현 알림 엔티티인 Notification, 알림 설정에 관련된 NotificationSetting 엔티티를 구현 Fixes: #54

---
+++ src/main/java/umc/duckmelang/domain/notificationsetting/domain/NotificationSetting.java
@@ -0,0 +1,38 @@
+package umc.duckmelang.domain.notificationsetting.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.duckmelang.domain.member.domain.Member;
+import umc.duckmelang.global.common.BaseEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class NotificationSetting extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notification_setting_id", nullable = false)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "member_id", unique = true, nullable = false)
+    private Member member;
+
+    @Column(nullable = false)
+    private Boolean chatNotificationEnabled = true;  // 채팅 알림 수신 여부
+    @Column(nullable = false)
+    private Boolean requestNotificationEnabled = true;  // 동행 확정 요청 알림 수신 여부
+    @Column(nullable = false)
+    private Boolean reviewNotificationEnabled = true;  // 후기 작성 알림 수신 여부
+
+
+    @Column(nullable = false)
+    private Boolean pushNotificationEnabled = true;  // 푸시 알림 수신 여부
+    @Column(nullable = false)
+    private Boolean smsNotificationEnabled = true;  // SMS 알림 수신 여부
+    @Column(nullable = false)
+    private Boolean emailNotificationEnabled = true;  // 이메일 알림 수신 여부
+}
