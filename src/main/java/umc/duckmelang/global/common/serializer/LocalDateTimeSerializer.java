81570c5b0f4defb946ac97c2181680f03d51859d
Sieun Park <park12010@naver.com>
2025-02-06 16:28:16 +0900

    Feat: 커스텀 직렬화기 생성, 직렬화기를 JacksonConfig에 추가
    LocalDateTime의 직렬화 형식을 지정하는 커스텀 직렬화기를 생성하고, 이를 JacksonConfig에 추가
    
    Fixes: #75

---
+++ src/main/java/umc/duckmelang/global/common/serializer/LocalDateTimeSerializer.java
@@ -0,0 +1,26 @@
+package umc.duckmelang.global.common.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeSerializer extends StdSerializer<LocalDateTime> {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    public LocalDateTimeSerializer() {
+        this(null);
+    }
+
+    public LocalDateTimeSerializer(Class<LocalDateTime> t) {
+        super(t);
+    }
+
+    @Override
+    public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        gen.writeString(value.format(formatter));
+    }
+}
