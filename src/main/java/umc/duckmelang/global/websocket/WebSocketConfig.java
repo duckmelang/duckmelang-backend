28393b57464f0a095dd9728c1607d8344bdecd96
Sieun Park <park12010@naver.com>
2025-02-16 12:54:26 +0900

    Chore: 오타 수정 wws -> wss
    오타 수정 wws -> wss
    
    Resolves: #88

--- src/main/java/umc/duckmelang/global/websocket/WebSocketConfig.java
+++ src/main/java/umc/duckmelang/global/websocket/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
         registry
                 // 클라이언트가 웹소켓 연결을 하고자 /wss/chat 경로로 연결을 시도하면 WebSocket 연결을 허용한다.
                 // ChatWebSocketHandler 클래스에서 이를 처리한다.
-                .addHandler(webSocketChatHandler, "/wws/chat")
+                .addHandler(webSocketChatHandler, "/wss/chat")
                 // CORS 설정
                 // 일단은 개발을 위하여 접속을 시도하는 모든 도메인, IP의 WebSocket 연결을 허용한다.
                 // 추후 특정 도메인만 허용하도록 설정히야 한다.
