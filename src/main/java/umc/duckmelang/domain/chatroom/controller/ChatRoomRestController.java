29a431d34eb170bd57821a4c52458ed7a51727ad
sjw <shinjw0131@gmail.com>
2025-02-16 15:16:59 +0900

    chore : add desccription of apis

--- src/main/java/umc/duckmelang/domain/chatroom/controller/ChatRoomRestController.java
+++ src/main/java/umc/duckmelang/domain/chatroom/controller/ChatRoomRestController.java
@@ -23,7 +23,7 @@ public class ChatRoomRestController {
 
     @GetMapping("")
     @CommonApiResponses
-    @Operation(summary = "특정 멤버가 참여한 채팅방 전체 목록 조회",description = "")
+    @Operation(summary = "특정 멤버가 참여한 채팅방 전체 목록 조회",description = "memberId는 임시로 사용하고 추후 jwt로 교체합니다. \n 이 api의 response로 넘기는 chatRoomId를 채팅방 세부 내역 조회 api의 request로 주시면 됩니다.\n status는 채팅방의 상태를 의미합니다")
     public ApiResponse<ChatRoomResponseDto.ChatRoomItemListDto> getChatRoomItemList(@RequestParam Long memberId,  // 임시로 사용. 나중에 JWT에서 추출할 예정
                                                                                     @RequestParam int page){
                                                                                     //TODO : 채팅방 목록 전체 조회
@@ -33,7 +33,7 @@ public class ChatRoomRestController {
 
     @GetMapping("/ongoing")
     @CommonApiResponses
-    @Operation(summary = "특정 멤버가 참여한 채팅방 중 진행중 목록 조회",description = "")
+    @Operation(summary = "특정 멤버가 참여한 채팅방 중 진행중 목록 조회",description = "memberId는 임시로 사용하고 추후 jwt로 교체합니다. \n 이 api의 response로 넘기는 chatRoomId를 채팅방 세부 내역 조회 api의 request로 주시면 됩니다.")
     public ApiResponse<ChatRoomResponseDto.ChatRoomItemListDto> getChatRoomOngoingItemList(@RequestParam Long memberId,  // 임시로 사용. 나중에 JWT에서 추출할 예정
                                                                                            @RequestParam int page){
         //TODO : 채팅방 목록 전체 조회
@@ -43,7 +43,7 @@ public class ChatRoomRestController {
 
     @GetMapping("/confirmed")
     @CommonApiResponses
-    @Operation(summary = "특정 멤버가 참여한 채팅방 중 동행확정 목록 조회",description = "")
+    @Operation(summary = "특정 멤버가 참여한 채팅방 중 동행확정 목록 조회",description = "memberId는 임시로 사용하고 추후 jwt로 교체합니다. \n 이 api의 response로 넘기는 chatRoomId를 채팅방 세부 내역 조회 api의 request로 주시면 됩니다.")
     public ApiResponse<ChatRoomResponseDto.ChatRoomItemListDto> getChatRoomConfirmedItemList(@RequestParam Long memberId,  // 임시로 사용. 나중에 JWT에서 추출할 예정
                                                                                              @RequestParam int page){
         //TODO : 채팅방 목록 전체 조회
@@ -53,7 +53,7 @@ public class ChatRoomRestController {
 
     @GetMapping("/terminated")
     @CommonApiResponses
-    @Operation(summary = "특정 멤버가 참여한 채팅방 중 종료 목록 조회",description = "")
+    @Operation(summary = "특정 멤버가 참여한 채팅방 중 종료 목록 조회",description = "memberId는 임시로 사용하고 추후 jwt로 교체합니다. \n 이 api의 response로 넘기는 chatRoomId를 채팅방 세부 내역 조회 api의 request로 주시면 됩니다.")
     public ApiResponse<ChatRoomResponseDto.ChatRoomItemListDto> getChatRoomTerminatedItemList(@RequestParam Long memberId,  // 임시로 사용. 나중에 JWT에서 추출할 예정
                                                                                               @RequestParam int page){
         //TODO : 채팅방 목록 전체 조회
